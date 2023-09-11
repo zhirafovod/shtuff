53b72f989badc6e6e102544817942a0cc4bd0467
Sergey Sergeev <sergey.sergeev@appdynamics.com>
2023-09-11 14:54:59 -0700

    an example js functions and class export and a function to import it dynamically

---
+++ src/module_exports.js
@@ -0,0 +1,21 @@
+class MyClass {
+    static foo() {
+        return 'Hello from MyClass';
+    }
+}
+
+class AnotherClass {
+    static bar() {
+        return 'Greetings from AnotherClass';
+    }
+}
+
+function myFunction() {
+    return 'Hello from myFunction';
+}
+
+function anotherFunction() {
+    return 'Greetings from anotherFunction';
+}
+
+export { MyClass, AnotherClass, myFunction, anotherFunction };
